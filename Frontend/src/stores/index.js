a75d97309a98c29e131406d4ed0bfb5fb4c6f48b
Pisetnea <Pobgsolo@gmail.com>
2026-03-20 14:40:28 +0700

    docs: change from feat/chef-dashboard-foreign

--- Frontend/src/stores/index.js
+++ Frontend/src/stores/index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 export * from './auth.store'
 export * from './ui.store'
 export * from './menu.store'
 
-=======
-export * from './auth.store'
-export * from './ui.store'
-export * from './menu.store'
-export * from './cart.store'
->>>>>>> 075116b688db32de46dfb4fe7b0059086cb04221
