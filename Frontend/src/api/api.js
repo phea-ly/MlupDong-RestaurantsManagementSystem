23fc1e7a782d3a2a5ab6b7c1860e0dc0dc0adcd5
Pisetnea <Pobgsolo@gmail.com>
2026-03-25 18:58:36 +0700

    fix: improve a litle feature

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,5 +1,6 @@
 // src/api/api.js
 import axios from 'axios'
+import { getToken, clearSession } from '@/utils/auth'
 
 const api = axios.create({
   baseURL:         import.meta.env.VITE_API_URL ?? 'http://127.0.0.1:8000/api',
