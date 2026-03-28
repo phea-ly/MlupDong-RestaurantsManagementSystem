2eff2b5f3f85a093e2c83521e5eb6d666e5ebfe3
RIN mesa <mesarin24@gmail.com>
2026-03-28 18:00:28 +0700

    improve performant

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: 'http://34.236.143.58:82/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
