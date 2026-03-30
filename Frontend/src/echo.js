638e8c9472255ddea546608c42d4ca9a31cd2e72
RIN mesa <mesarin24@gmail.com>
2026-03-30 20:17:14 +0700

    pull code form develop

--- Frontend/src/echo.js
+++ Frontend/src/echo.js
@@ -1,4 +1,3 @@
-
 import Echo   from 'laravel-echo'
 import Pusher from 'pusher-js'
 
@@ -8,7 +7,7 @@ export const echo = new Echo({
   broadcaster:       'reverb',
   key:               import.meta.env.VITE_REVERB_APP_KEY,
   wsHost:            import.meta.env.VITE_REVERB_HOST,
-  wsPort:            Number(import.meta.env.VITE_REVERB_PORT) || 8080,
+  wsPort:            Number(import.meta.env.VITE_REVERB_PORT) || 82,
   wssPort:           Number(import.meta.env.VITE_REVERB_PORT) || 443,
   forceTLS:          (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
   enabledTransports: ['ws', 'wss'],
